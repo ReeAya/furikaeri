006a45c2b6c09fb7532498ce98c551e789b01d78
reezan_s <reezansmith@gmail.com>
2024-09-11 16:11:04 +0900

    Loop through list data and changed react-icons

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,17 +1,17 @@
-import { MdPostAdd, MdMessage } from "react-icons/md";
-
+import { MdPostAdd, MdEditNote } from "react-icons/md";
+import { IoMdAdd } from "react-icons/io";
 import classes from "./MainHeader.module.css";
 
 function MainHeader({ onCreatePost }) {
   return (
     <header className={classes.header}>
       <h1 className={classes.logo}>
-        <MdMessage />
+        <MdEditNote />
         Furikaeri
       </h1>
       <p>
         <button className={classes.button} onClick={onCreatePost}>
-          <MdPostAdd size={18} />
+          <IoMdAdd size={18} />
           New Post
         </button>
       </p>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -23,16 +23,24 @@ export default function PostsList({ modalVisible, onStopPosting }) {
     <>
       {modalVisible ? modalContent : null}
 
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-          <Post
-            author={post.author}
-            feeling={post.feeling}
-            body={post.body}
-            key={uuidv4()}
-          />
-        ))}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post
+              author={post.author}
+              feeling={post.feeling}
+              body={post.body}
+              key={uuidv4()}
+            />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center" }}>
+          <h2>There are no posts!</h2>
+          <p>Let's add some posts!</p>
+        </div>
+      )}
     </>
   );
 }
